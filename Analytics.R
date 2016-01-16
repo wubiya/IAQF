b45d2b1fe1879ca606d7a64ba2214a64d38cccd1
wubiya <wubiya929@163.com>
2016-01-16 15:54:47 -0500

    roughly regression

--- Analytics.R
+++ Analytics.R
@@ -7,3 +7,25 @@ oil <- oil[-1,]
 colnames(oil) <- c("Date", "Price")
 oil$Date <- as.Date(as.character(oil$Date))
 oil$Price <- as.numeric(levels(oil$Price))[oil$Price]
+oil$Chg <- numeric(length(oil$Date))
+oil$Chg[-1] <- diff(oil$Price)/oil$Price[-length(oil$Price)]
+
+filename <- "/Users/wubiya/Dropbox/IAQF 2016/2016/Data/BB_Industry_Level_Aggregates.xlsx"
+industry <- read.xls (filename, sheet = 7, header = TRUE)
+industry <- industry[-1,]
+colnames(industry) <- c("Date", "Price")
+industry$Date <- as.Date(as.character(industry$Date))
+industry$Price <- as.numeric(levels(industry$Price))[industry$Price]
+industry$Chg <- numeric(length(industry$Date))
+industry$Chg[-1] <- diff(industry$Price)/industry$Price[-length(industry$Price)]
+
+y <- industry$Chg[is.element(industry$Date, oil$Date)]
+x <- oil$Chg[is.element(oil$Date, industry$Date)]
+
+regdata<- data.frame(y[-1],x[-length(x)])
+colnames(regdata) <- c("InduPrice","OilPrice")
+ls<-lm(regdata[5500:6000,])
+summary(ls)
+
+
+
