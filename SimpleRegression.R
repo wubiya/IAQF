2a4f9ae8d7a8acecbb052b2daef62d699847deb0
wubiya <wubiya929@163.com>
2016-02-03 17:16:51 -0500

    add regression on industry on swf

--- SimpleRegression.R
+++ SimpleRegression.R
@@ -9,6 +9,8 @@ load(paste0(filepath,"OIL.Rdata"))
 load(paste0(filepath,"INDEX.Rdata"))
 load(paste0(filepath,"sp500.Rdata"))
 load(paste0(filepath,"SWF.Rdata"))
+load(paste0(filepath,"SWF2.Rdata"))
+load(paste0(filepath,"oil_spot.Rdata"))
 AddReturn<-function(DataFrame)
 {
   len_ = length(DataFrame$Date)
@@ -70,6 +72,7 @@ simple_regression_industry<-function(sdate,tdate,inducol,oilcol,spcol){
     
     ls = lm(regdata)
     summ <- summary(ls)
+    summ$coefficients <- unclass(coeftest(lms, vcov. = NeweyWest))
     cat(industry.namelist[i],unname(summ$coefficients[-1,3]),'\n',sep=",")
     
   }
@@ -95,7 +98,6 @@ simple_regression_swf<-function(sdate,tdate){
   for(i in 1:6){
     swf<-swf.list[[i]]
     swf<-MonthData(swf)
-    write.csv(swf, paste0("~/Dropbox/IAQF/code/csvfile/",swf.namelist[i],".csv"))
     subswf<-swf[swf$Date>= sdate & swf$Date <= tdate & swf$Date %in% oil$Date,]
     suboil <- oil[oil$Date%in%subswf$Date,]
     
@@ -104,6 +106,7 @@ simple_regression_swf<-function(sdate,tdate){
     
     lms = lm(regdata)
     summ <- summary(lms)
+    summ$coefficients <- unclass(coeftest(lms, vcov. = NeweyWest))
     cortest<-cor.test(subswf$PX_LAST,suboil$PX_LAST)
     cat(swf.namelist[i],unname(summ$coefficients[-1,3]),unname(summ$coefficients[-1,4]), summ$df[2], cortest$p.value,cortest$estimate, '\n',sep=",")
     
@@ -113,7 +116,7 @@ simple_regression_swf<-function(sdate,tdate){
 }
 
 
-simple_regression_industry<-function(sdate,tdate){
+simple_regression_industry_swf<-function(sdate,tdate){
   # sdate,tdate is the start/end date of regression, format is like "2015-1-1"
   # inducol is the column you want to use to represent return, including frequency of day,week,and month 
   # which is represented by LOG_RTN_1D,LOG_RTN_1W,LOG_RTN_1M
@@ -124,23 +127,33 @@ simple_regression_industry<-function(sdate,tdate){
     file.remove(paste0("~/Dropbox/IAQF/code/csvfile/",sdate," to ",tdate,'.csv'))
   
   sink(paste0("~/Dropbox/IAQF/code/csvfile/",sdate," to ",tdate,'.csv'),append=TRUE)
-  cat("SWF","Oil_CUR","p-value","free degree","cor","cor.test",'\n',sep=",")
+  cat("Indu","Oil","SP500","SWF","have influence or not",'\n',sep=",")
   sdate<-as.yearmon(as.Date(sdate))
   tdate<-as.yearmon(as.Date(tdate))
-  for(i in 1:6){
-    swf<-swf.list[[i]]
-    swf<-MonthData(swf)
-    write.csv(swf, paste0("~/Dropbox/IAQF/code/csvfile/",swf.namelist[i],".csv"))
-    subswf<-swf[swf$Date>= sdate & swf$Date <= tdate & swf$Date %in% oil$Date,]
-    suboil <- oil[oil$Date%in%subswf$Date,]
+  for(i in 1:24){
     
-    regdata<- data.frame(subswf$PX_LAST,suboil$PX_LAST)
-    colnames(regdata) <-c("SWF","Oil_CUR")
+    industry<-industry.list[[i]]
+    industry<-MonthData(industry)
     
-    lms = lm(regdata)
+    subindustry<-industry[industry$Date>= sdate & industry$Date <= tdate & industry$Date %in% oil$Date,]
+    suboil <- oil[oil$Date%in%subindustry$Date,]
+    subswf2 <- swf2[swf2$Date%in%subindustry$Date,]
+    subsp500 <- sp500[sp500$Date%in%subindustry$Date,]
+    
+    
+    regdata<- data.frame(subindustry$LOG_RTN,suboil$LOG_RTN,subsp500$LOG_RTN, subswf2$LOG_RTN)
+    colnames(regdata) <-c("Indu","Oil","SP500","SWF")
+    lms_ref <-lm(Indu ~ Oil+SP500, regdata)
+    summ_ref <- summary(lms_ref)
+    summ_ref$coefficients <- unclass(coeftest(lms_ref, vcov. = NeweyWest))
+    slope_ref <- summ_ref$coefficients[3]
+    lms <- lm(Indu~Oil+SP500+SWF, regdata)
     summ <- summary(lms)
-    cortest<-cor.test(subswf$PX_LAST,suboil$PX_LAST)
-    cat(swf.namelist[i],unname(summ$coefficients[-1,3]),unname(summ$coefficients[-1,4]), summ$df[2], cortest$p.value,cortest$estimate, '\n',sep=",")
+    summ$coefficients <- unclass(coeftest(lms, vcov. = NeweyWest))
+    lms2 <- lm(Indu~Oil+SP500+SWF, offset=slope_ref*SP500, regdata)
+    summ2 <- summary(lms2)
+    summ2 <-unclass(coeftest(lms2, vcov. = NeweyWest))
+    cat(industry.namelist[i],unname(summ$coefficients[-1,3]),summ2[2,4]<0.05,'\n',sep=",")
     
   }
   sink(file=NULL)
@@ -149,31 +162,45 @@ simple_regression_industry<-function(sdate,tdate){
 
 
 
+# -----------------------------------------------------------
 
 sp500<-AddReturn(sp500)
-oil<-oil.list[[1]]
+oil<-oil_spot.list[[1]]
 oil<-AddReturn(oil)
-
 sp500<-sp500[sp500$Date%in%oil$Date,]
 oil<-oil[oil$Date%in%sp500$Date,]
-
 sdate<-"2009-1-1"
 tdate<-"2014-1-1"
 inducol <- "LOG_RTN_1D"
 oilcol <- c("LOG_RTN_1D","LOG_RTN_1D_LAG")
 spcol <-c("LOG_RTN_1D","LOG_RTN_1D_LAG")
-
-
 simple_regression_industry(sdate,tdate,inducol,oilcol,spcol)
 
 
+# -----------------------------------------------------------
 
-
-oil<-oil.list[[1]]
+oil<-oil_spot.list[[1]]
 oil<-MonthData(oil)
 sdate<-"2014-9-1"
 tdate<-"2016-2-1"
 simple_regression_swf(sdate,tdate)
+
+# -----------------------------------------------------------
+
+swf2<-swf2.list[[1]]
+swf2<-MonthData(swf2)
+oil<-oil_spot.list[[1]]
+oil<-MonthData(oil)
+sp500<-MonthData(sp500)
+swf2<-swf2[swf2$Date%in%oil$Date & swf2$Date%in%sp500$Date,]
+oil<-oil[oil$Date%in%swf2$Date & oil$Date%in%sp500$Date,]
+sp500<-sp500[sp500$Date%in%swf2$Date & sp500$Date%in%oil$Date,]
+sdate<-"2012-1-1"
+tdate<-"2014-1-1"
+simple_regression_industry_swf(sdate,tdate)
+
+
+
 
 
 
